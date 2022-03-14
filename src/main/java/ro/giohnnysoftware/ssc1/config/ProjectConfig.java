208673b92ba799673c00aed7d316e723c0572c96
GIOhnny <GIOhnny@gmail.com>
2022-03-14 22:23:03 +0200

    Initial commit https://www.youtube.com/watch?v=Of4HFbsPKqk&list=PLEocw3gLFc8XRaRBZkhBEZ_R3tmvfkWZz

---
+++ src/main/java/ro/giohnnysoftware/ssc1/config/ProjectConfig.java
@@ -0,0 +1,33 @@
+package ro.giohnnysoftware.ssc1.config;
+
+import org.springframework.cglib.proxy.NoOp;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var usd = new InMemoryUserDetailsManager();
+
+        var u1 = User.withUsername("bill")
+                .password("12345")
+                .authorities("read")
+                .build();
+
+        usd.createUser(u1);
+
+        return usd;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
